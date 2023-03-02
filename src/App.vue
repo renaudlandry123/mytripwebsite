56517412c0872d4cb8ef0bb5351d571f667cbf0d
renaudlandry123 <115116385+renaudlandry123@users.noreply.github.com>
2023-03-02 14:43:45 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <nav>
+    <router-link to="/" class="nav-link">Accueil</router-link>
+    <router-link to="/about" class="nav-link">Réservation</router-link>
+    <router-link to="/i" class="nav-link">Maison</router-link>
+    <router-link to="/d" class="nav-link">Terrain</router-link>
+  </nav>
+  <router-view />
+</template>
+
+<script>
+export default {
+  methods: {
+    refreshPage() {
+      location.reload();
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.refresh-text {
+  cursor: pointer;
+}
+
+body {
+  background: white;
+}
+
+nav #img {
+  position: left;
+  width: 50px;
+  height: 50px;
+}
+
+.nav-link {
+  text-decoration: none;
+  color: white;
+  font-weight: bold;
+  padding: 30px;
+}
+
+nav {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: rgba(128, 128, 128, 0.5);
+}
+
+.nav-link:hover {
+  text-decoration: underline;
+  text-decoration-color: black;
+  text-underline-offset: 5px;
+  text-decoration-thickness: 3px;
+  border-radius: 20px;
+}
+
+.nav-link.router-link-active {
+  text-decoration: underline;
+  text-decoration-color: black;
+  text-underline-offset: 5px;
+  text-decoration-thickness: 3px;
+  border-radius: 20px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="accueil">
+    <img id="ImageAccueil" src="../assets/imageAccueil.jpg" />
+    <h1 style="font-family: 'Courier New', Courier, monospace">
+      Les offres de la semaine !!!
+    </h1>
+    <div class="image-grid">
+      <img src="../assets/image1.jpg" />
+      <img src="../assets/image2.jpg" />
+      <img src="../assets/image3.jpg" />
+      <img src="../assets/image4.jpg" />
+      <img src="../assets/image5.jpg" />
+      <img src="../assets/image6.jpg" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+};
+</script>
+
+<style scoped>
+#ImageAccueil {
+  top: 0;
+  left: 0;
+  z-index: -1;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.image-grid {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding-top: 10px;
+}
+
+.image-grid img {
+  width: 25%;
+  height: 200px;
+  width: 350px;
+  margin: 20px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+}
+</style>
