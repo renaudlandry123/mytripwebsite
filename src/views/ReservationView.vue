f1578752263f8d3c63b6964af2f3f40892fd23aa
renaudlandry123 <115116385+renaudlandry123@users.noreply.github.com>
2023-03-07 17:28:01 -0500

    Update and rename AboutView.vue to ReservationView.vue

---
+++ src/views/ReservationView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="Reservation">
+    <ReservationComponent />
+  </div>
+</template>
+
+<script>
+import ReservationComponent from "@/components/ReservationComponent.vue";
+
+export default {
+  name: "ReservationView",
+  components: {
+    ReservationComponent,
+  },
+};
+</script>
